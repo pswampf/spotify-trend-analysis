d20634c19742b373f802e1e14a231c8d878bf514
pswampf <135308626+pswampf@users.noreply.github.com>
2023-06-02 02:52:13 -0400

    Update and rename spotifyConstants_template.py to spotifyConstants.py

---
+++ GeneralizedSpotifyAnalyser/spotifyConstants.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 23 14:21:46 2020
+
+@author: olehe
+
+Fill in your API info here, and rename to spotifyConstants.py
+"""
+
+myUser = 'carolinequigley'
+myClientID = 'f53ff10d14824c17b0fd8ad3d6779cc2'
+myClientSecret = '8a1b1ce5327842258d6c7a8d0f0fd8b8'
+myRedirect = 'http://example.com/callback/'
